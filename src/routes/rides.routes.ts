0130aff090a045240818f842e0e25d41d9dcc599
Joshua Mbafor <joshua.mbafor@amalitechtraining.org>
2026-09-11 16:29:48 +0000

    feat: wire GET /rides route

--- src/routes/rides.routes.ts
+++ src/routes/rides.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
-import { createRide } from '../controllers/rides.controller';
+import { createRide, listRides } from '../controllers/rides.controller';
 import { requireAuth } from '../middlewares/requireAuth.middleware';
 
 export const ridesRouter = Router();
 
+ridesRouter.get('/rides', listRides);
 ridesRouter.post('/rides', requireAuth, createRide);
