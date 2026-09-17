407e23328a09ce59c71c83a3b62f2b3dc296f800
Joshua Mbafor <joshua.mbafor@amalitechtraining.org>
2026-09-17 14:49:09 +0000

    feat: request to join a ride

--- src/routes/rides.routes.ts
+++ src/routes/rides.routes.ts
@@ -1,11 +1,43 @@
 import { Router } from 'express';
 
+import {
+  acceptRequest,
+  createRequest,
+  declineRequest,
+  listRideRequests,
+} from '../controllers/rideRequests.controller';
 import { createRide, listRides } from '../controllers/rides.controller';
 import { requireAuth } from '../middlewares/auth.middleware';
 import { validate } from '../middlewares/validate.middleware';
+import { requestIdParamsSchema, rideIdParamsSchema } from '../validators/rideRequests.validator';
 import { createRideSchema, listRidesSchema } from '../validators/rides.validator';
 
 export const ridesRouter = Router();
 
 ridesRouter.get('/', requireAuth, validate({ query: listRidesSchema }), listRides);
 ridesRouter.post('/', requireAuth, validate({ body: createRideSchema }), createRide);
+
+ridesRouter.post(
+  '/:rideId/requests',
+  requireAuth,
+  validate({ params: rideIdParamsSchema }),
+  createRequest,
+);
+ridesRouter.get(
+  '/:rideId/requests',
+  requireAuth,
+  validate({ params: rideIdParamsSchema }),
+  listRideRequests,
+);
+ridesRouter.patch(
+  '/:rideId/requests/:requestId/accept',
+  requireAuth,
+  validate({ params: requestIdParamsSchema }),
+  acceptRequest,
+);
+ridesRouter.patch(
+  '/:rideId/requests/:requestId/decline',
+  requireAuth,
+  validate({ params: requestIdParamsSchema }),
+  declineRequest,
+);
